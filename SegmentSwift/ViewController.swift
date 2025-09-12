f89f70631e5125ad0125d44f60e7d0273c214a2f
Ruben Charles <ruben.charles2508@gmail.com>
2025-09-13 00:30:08 +0530

    Initial Commit

---
+++ SegmentSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentSwift
+//
+//  Created by Ruben Charles on 13/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SegmentSwiftTests/SegmentSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentSwiftTests.swift
+//  SegmentSwiftTests
+//
+//  Created by Ruben Charles on 13/09/25.
+//
+
+import Testing
+@testable import SegmentSwift
+
+struct SegmentSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
