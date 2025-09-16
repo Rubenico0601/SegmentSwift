42b61a54eb68004994c3672f49866db77ae99157
Ruben Charles <ruben.charles2508@gmail.com>
2025-09-17 01:55:44 +0530

    first commit

---
+++ SegmentSwift/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SegmentSwift
+//
+//  Created by Ruben Charles on 13/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
